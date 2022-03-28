0b153eab40d96671e7968e32f3a842da23c49448
Joe Deering <JDeering94@gmail.com>
2022-03-28 14:25:04 +0100

    added login component

---
+++ src/api/auth.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const loginUser = async (credentials) => {
+  const options = {
+    method: 'POST',
+    url: '/api/login',
+    data: credentials,
+  };
+
+  const { data } = await axios.request(options);
+  if (data.token) {
+    window.sessionStorage.setItem('token', data.token);
+  } else {
+    window.sessionStorage.removeItem('token');
+  }
+  console.log(data);
+  return data.message;
+};
--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import Home from './Home.js';
 
 import '../styles/style.scss';
 import FilmIndex from './FilmIndex';
+import Login from './auth/Login.js';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/FilmIndex" element={<FilmIndex />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </BrowserRouter>
 );
