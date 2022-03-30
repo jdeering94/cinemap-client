46befa92be5e5f5a9837a2d3daa1b458a24c8c54
Gabriel <gabriel_arafa@hotmail.com>
2022-03-30 16:09:40 +0100

    add draft content for about page; add class selector to ul in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ const Navbar = () => {
       {show && (
         <div>
           <div>
-            <ul>
+            <ul className="navlist">
               <li>
                 <Link onClick={handleClose} to="/">
                   Home
