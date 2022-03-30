ad452eee9de4322aee2c80c38ff14d65e2451bba
Gabriel <gabriel_arafa@hotmail.com>
2022-03-30 20:11:23 +0100

    move film location below photo to add more space for titile

--- src/components/FilmIndex.js
+++ src/components/FilmIndex.js
@@ -28,25 +28,24 @@ const FilmIndex = () => {
             {films.map((film) => (
               <div key={film._id} className="column card m-3 is-one-fifth">
                 <Link to={`/film/${film._id}`}>
-                  <h2 className="card-header">
-                    {film.title} - {film.country}
-                  </h2>
+                  <h2 className="card-header">{film.title}</h2>
                   <div className="card-image">
                     <figure className="image is-4by5">
                       <img src={film.img} alt={film.title} />
                     </figure>
-                    <div className="card-footer">
-                      {film.comments.length > 0 && (
-                        <p className="card-footer-item">
-                          {averageRating(film).toFixed(1)} / 10 ⭐️
-                        </p>
-                      )}
-                      {film.likedBy.length > 0 && (
-                        <p className="card-footer-item">
-                          💙 {film.likedBy.length}
-                        </p>
-                      )}
-                    </div>
+                  </div>
+                  <p className="card-footer">{film.country}</p>
+                  <div className="card-footer">
+                    {film.comments.length > 0 && (
+                      <p className="card-footer-item">
+                        {averageRating(film).toFixed(1)} / 10 ⭐️
+                      </p>
+                    )}
+                    {film.likedBy.length > 0 && (
+                      <p className="card-footer-item">
+                        💙 {film.likedBy.length}
+                      </p>
+                    )}
                   </div>
                 </Link>
               </div>
