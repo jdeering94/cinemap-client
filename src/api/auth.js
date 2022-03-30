8da280a9d355cd54f35586976c4cc8a3525696d4
Dani Gordon <lilyluvr723@gmail.com>
2022-03-30 20:27:55 +0200

    added logout.worked on navbar shit

--- src/api/auth.js
+++ src/api/auth.js
@@ -41,3 +41,19 @@ export const getCurrentUserById = async (userId) => {
   const { data } = await axios.request(options);
   return data;
 };
+
+export const getToken = () => {
+  return window.sessionStorage.getItem('token');
+};
+
+export const setToken = (token) => {
+  window.sessionStorage.setItem('token', token);
+};
+
+export const removeToken = () => {
+  window.sessionStorage.removeItem('token');
+};
+
+export const removeUserId = () => {
+  window.sessionStorage.removeItem('userId');
+};
