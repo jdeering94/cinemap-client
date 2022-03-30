04701002dbfd344c6165a04900e312b371488360
Joe Deering <JDeering94@gmail.com>
2022-03-30 17:02:11 +0100

    added random film component

--- src/components/RandomFilm.js
+++ src/components/RandomFilm.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getRandomFilm } from '../api/films';
 
 const RandomFilm = () => {
-  return <h1>This is the page for a Random Film</h1>;
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    const getData = async () => {
+      const randomFilmObject = await getRandomFilm();
+      navigate(`/film/${randomFilmObject[0]._id}`);
+    };
+    getData();
+  }, []);
+
+  return <h1>Loading...</h1>;
 };
 
 export default RandomFilm;
