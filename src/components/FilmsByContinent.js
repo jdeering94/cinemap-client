0850238901ca44a7435c93633dc8fe6e258ac734
Dani Gordon <lilyluvr723@gmail.com>
2022-03-31 13:12:50 +0200

    all film pgs are uniform

--- src/components/FilmsByContinent.js
+++ src/components/FilmsByContinent.js
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { getAllFilmsByContinent } from '../api/films';
 import { averageRating } from '../lib/ratingFunctions';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { flag } from 'country-emoji';
+import { deleteFilm } from '../api/films';
+import { isAdmin } from '../lib/authentication';
 
 const FilmsByContinent = () => {
   const { continent } = useParams();
@@ -20,24 +25,56 @@ const FilmsByContinent = () => {
     getData();
   }, []);
   console.log(films);
+
+  const handleDeleteFilm = async (filmId) => {
+    if (isAdmin()) {
+      await deleteFilm(filmId);
+      const films = await getAllFilmsByContinent();
+      setFilm(
+        films.sort((a, b) =>
+          a.title > b.title ? 1 : b.title > a.title ? -1 : 0
+        )
+      );
+    } else {
+      window.alert('You must be an Admin to delete a Film');
+    }
+  };
+
   return (
     <>
-      <h1 className="title">Films from {continent}</h1>
+      <h1 className="title has-text-centered">Films from {continent}</h1>
       {films ? (
         <div className="container">
           <div className="columns is-multiline">
             {films.map((film) => (
               <div key={film._id} className="column card m-3 is-one-fifth">
+                {isAdmin() && (
+                  <FontAwesomeIcon
+                    onClick={() => handleDeleteFilm(film._id)}
+                    className="x-mark"
+                    icon={faXmark}
+                  />
+                )}
                 <Link to={`/film/${film._id}`}>
-                  <h2 className="card-header">
-                    {film.title} - {film.country}
-                  </h2>
                   <div className="card-image">
                     <figure className="image is-4by5">
                       <img src={film.img} alt={film.title} />
                     </figure>
+                  </div>
+                  <div className="is-flex is-justify-content-space-between">
+                    <p className="">{film.title}</p>
+                    <p className="">{flag(`${film.country}`)}</p>
+                  </div>
+                  <div className="card-footer">
                     {film.comments.length > 0 && (
-                      <p>{averageRating(film)} ⭐️</p>
+                      <p className="card-footer-item">
+                        {averageRating(film).toFixed(1)} ⭐️
+                      </p>
+                    )}
+                    {film.likedBy.length > 0 && (
+                      <p className="card-footer-item">
+                        💙 {film.likedBy.length}
+                      </p>
                     )}
                   </div>
                 </Link>
